9b554e0291b22549b23b45f560e48c8f1ff913ce
surmon-china <surmon@foxmail.com>
2016-12-04 02:02:09 +0800

    Category Batch delete bug todu...

--- src/app/pages/article/components/category/category.component.ts
+++ src/app/pages/article/components/category/category.component.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { ModalDirective } from 'ng2-bootstrap';
 import { FormGroup, AbstractControl, FormBuilder, Validators } from '@angular/forms';
-
+import { NotificationsService } from 'angular2-notifications';
 import { ArticleCategoryService } from './category.service';
 
 @Component({
@@ -14,8 +14,12 @@ export class ArticleCategory {
 
   public categories = { data: [] };
   public addCategoryState = { ing: false, success: false };
+  public delCategory:any;
+  public editCategory:any;
+  public delCategories:any;
 
-  constructor(private _articleCategoryService:ArticleCategoryService) {}
+  constructor(private _articleCategoryService:ArticleCategoryService,
+              private _notificationsService: NotificationsService) {}
 
   ngOnInit() {
     this._getCategories();
@@ -65,23 +69,58 @@ export class ArticleCategory {
     this.addCategoryState = { ing: true, success: false };
     this._articleCategoryService.addCategory(category).then(_category => {
       if(_category.code) this._getCategories();
-      this.addCategoryState = { ing: false, success: true };
+      this.addCategoryState = { ing: false, success: !!_category.code };
     });
   }
 
   // 修改分类
   private _putCategory(category) {
-    console.log(category);
+    this.editCategory = category;
   }
 
-  // 删除分类
+  // 确认修改分类
+  private _doPutCategory(category) {
+    this.addCategoryState = { ing: true, success: false };
+    this._articleCategoryService.putCategory(Object.assign(this.editCategory, category)).then(category => {
+      if(category.code) this._getCategories();
+      this.addCategoryState = { ing: false, success: !!category.code };
+    });
+  }
+
+  // 删除分类弹窗
   private _delCategory(category) {
-    console.log(category);
+    this.delCategory = category;
     this.delModal.show();
+  }
+
+  // 分类弹窗取消
+  private _canceldDelCategory(category) {
+    this.delCategory = null;
+    this.delModal.hide();
+  }
+
+  // 确认删除分类
+  private _doDelCategory() {
+    this._articleCategoryService.delCategory(this.delCategory._id).then(category => {
+      this.delCategory = null;
+      this.delModal.hide();
+      this._getCategories();
+    });
   }
 
   // 批量删除分类
   private _delCategories(categories) {
-    console.log(this);
+    this.delCategories = categories;
+    this.delCategory = null;
+    this.delModal.show();
+  }
+
+  // 确认批量删除
+  private _doDelCategories() {
+    this._articleCategoryService.delCategories(this.delCategories).then(categories => {
+      this.delCategories = null;
+      this.delModal.hide();
+      this._getCategories();
+    });
   }
 }
