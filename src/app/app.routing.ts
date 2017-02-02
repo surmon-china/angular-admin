4a76ccfd3b731deaa44dd4dad5c3344bfa747403
surmon <surmon@foxmail.com>
2017-02-02 17:20:13 +0800

    add branch release

--- src/app/app.routing.ts
+++
@@ -1,8 +0,0 @@
-import { Routes, RouterModule } from '@angular/router';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: '**', redirectTo: 'dashboard' }
-];
-
-export const routing = RouterModule.forRoot(routes, { useHash: false });
--- src/app/theme/pipes/dateHandle/dateHandle.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({name: 'dataToLocale'})
-export class DataToLocalePipe implements PipeTransform {
-
-  transform(input: any): string {
-    return new Date(input).toLocaleString();
-  }
-}
