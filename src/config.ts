0ad0ea86ff30f4831fac5cce37705d925ae06321
surmon-china <surmon@foxmail.com>
2017-02-11 02:00:29 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -2,15 +2,5 @@ const devApi = '/api';
 const prodApi = 'https://api.surmon.me';
 const staticApi = 'https://static.surmon.me';
 
-export const API_ROOT = (function(){ 
-	try { 
-		if(process && process.env.ENV == "development") { 
-			return devApi 
-		} else { 
-			return prodApi 
-		}
-	} catch(err) { 
-		return prodApi 
-	}
-})();
+export const API_ROOT = prodApi;
 export const STATIC_URL = staticApi;
