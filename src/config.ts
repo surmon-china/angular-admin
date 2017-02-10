d9eb1a6d7e99d9b6fd77c93469a66b429ec2fa20
surmon <surmon@foxmail.com>
2017-02-11 01:40:25 +0800

    publish

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,15 @@ const devApi = '/api';
 const prodApi = 'https://api.surmon.me';
 const staticApi = 'https://static.surmon.me';
 
-export const API_ROOT = (function(){ try { if(process) { return devApi } else { return prodApi }} catch(err) { return prodApi }})();
+export const API_ROOT = (function(){ 
+	try { 
+		if(process && process.env.ENV == "development") { 
+			return devApi 
+		} else { 
+			return prodApi 
+		}
+	} catch(err) { 
+		return prodApi 
+	}
+})();
 export const STATIC_URL = staticApi;
