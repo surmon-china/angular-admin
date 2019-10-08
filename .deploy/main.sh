b9d9e8c94098ce13bbf2f9dd9ce3e67c58df0063
surmon-china <surmon@foxmail.com>
2019-10-08 13:14:37 +0800

    Update deploy script

--- .deploy/main.sh
+++ .deploy/main.sh
@@ -7,6 +7,8 @@ cd ./product
 
 echo "[deploy] Delete files..."
 ls | grep -v dist.tar | xargs rm -r
-echo "[deploy] Decompression files..."
-tar -xvf dist.tar
+echo "[deploy] Decompression dist.tar..."
+tar -xvf dist.tar -C ./
+echo "[deploy] Delete dist.tar..."
+rm dist.tar
 echo "[deploy] Finished."
