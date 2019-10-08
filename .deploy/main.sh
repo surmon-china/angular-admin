126c0e779ebae327326d1cda42d5b75ccd8b4b94
surmon-china <surmon@foxmail.com>
2019-10-08 15:40:07 +0800

    Update deploy and actions

--- .deploy/main.sh
+++ .deploy/main.sh
@@ -3,12 +3,18 @@
 SHELL_PATH=$(dirname $0)
 cd $WEB_PATH
 cd ..
+
+echo "[deploy] pulling source code..."
+git fetch --all && git reset --hard origin/master && git pull
+git checkout master
+
 cd ./product
 
-echo "[deploy] Delete files..."
-ls | grep -v dist.tar | xargs rm -r
-echo "[deploy] Decompression dist.tar..."
-tar -xvf dist.tar -C ./
-echo "[deploy] Delete dist.tar..."
-rm dist.tar
+echo "[deploy] Delete old files..."
+rm -r ./www
+mv ./bak.dist.tar.gz ./prev.bak.dist.tar.gz
+echo "[deploy] Decompression tar..."
+tar -xvf dist.tar.gz -C ./www
+echo "[deploy] Rename tar..."
+mv ./dist.tar.gz ./bak.dist.tar.gz
 echo "[deploy] Finished."
